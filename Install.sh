37ff02d4ac11276e3c946baca8f9cdaf70985cc5
Praful Shrivastava <107274773+Praful1510@users.noreply.github.com>
2022-12-11 12:55:55 -0800

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+gn='\033[32m'
+bl='\033[34m'
+clr="\033[31m"
+noclr="\033[0m"
+
+
+echo -e "\n${clr}"
+echo -e "\t\t#     #              #####                                           "
+echo -e "\t\t##   ##             #     # ######  ####  #    # #####  # ##### #   #"
+echo -e "\t\t# # # #             #       #      #    # #    # #    # #   #    # # "
+echo -e "\t\t#  #  #    #####     #####  #####  #      #    # #    # #   #     #  "
+echo -e "\t\t#     #                   # #      #      #    # #####  #   #     #  "
+echo -e "\t\t#     #             #     # #      #    # #    # #   #  #   #     #  "
+echo -e "\t\t#     #              #####  ######  ####   ####  #    # #   #     #  "
+echo -e "${noclr}\t\t${bl}_____________________________________________________________________${noclr}"
+echo -e "\n"
+echo -e "\t\t\t\t\t\t\t\t\t\t${gn}BY Praful @SecuritySwitch"
+echo -e "${noclr}\n"
+
+if [[ $EUID -eq 0 ]]; then
+    # Download Burp Suite Profesional Latet Version
+    echo 'Downloading Burp Suite Professional v2022.8.2 ....'
+    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.8.2&type=jar"
+    wget "$Link" -O Burp_Suite_Pro.jar --quiet --show-progress
+    sleep 2
+
+    # execute Keygenerator
+    echo 'Starting Keygenerator'
+    (java -jar keygen.jar) &
+    sleep 3s
+    
+    # Execute Burp Suite Professional with Keyloader
+    echo 'Executing Burp Suite Professional with Keyloader'
+    echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/Burp_Suite_Pro.jar &" > burp
+    chmod +x burp
+    cp burp /bin/burp 
+    (./burp)
+else
+    echo "Execute Command as Root User"
+    exit
+fi
+
+# Lets Download the latest Burp Suite Professional jar File
+echo "`n`t 1. Please download latest Burp Suite Professional Jar file from :-:"
+echo "`n`t https://portswigger.net/burp/releases/startdownload?product=pro&version=&type=Jar"
+echo "`n`t 2. Replace the existing Burp-Suite-Pro.jar file with downloaded jar file. `n`t Keep previous file name"
